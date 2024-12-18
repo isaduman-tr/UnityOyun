7a3cae6586241e5950183b3ba2add1b50aa15b1d
isa duman <114006114+isaduman-tr@users.noreply.github.com>
2024-12-18 16:00:05 +0300

    tt

--- My project/Assets/scripts/Elements/Enemy.cs
+++ My project/Assets/scripts/Elements/Enemy.cs
@@ -12,26 +12,32 @@ public class Enemy : MonoBehaviour
     public float speed;
     private Rigidbody _rb;
     public NavMeshAgent navMeshAgent;
+    private Animator _animator;
+    private bool _isWalking;
     
 
     public void StartEnemy(Player player)
     {
         _player = player;
         _rb = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
     }
     private void Update()
     {
         if (_player.ÝsAppleCollected)
         {
-            /* var direction = (_player.transform.position - transform.position).normalized; //Enemynin düþmana doðru konumlanmasý
-            direction.y=0;  
-            _rb.position += direction * Time.deltaTime * speed; // Enemy hýzý */
+            
             navMeshAgent.destination = _player.transform.position;  //ai navigation tarafýndan enemynin playeri takip etmesi
+            if (!_isWalking) // booller her zaman false baþlýyor bir sonraki update de içine giremeyecek ders 16 dakika 17
+            {
+                _isWalking = true;
+                _animator.SetTrigger("Walk");
+            }
         }
     }
     public void Stop() 
     {
-        // speed = 0; //Collactables toplanýnca durmasý
         navMeshAgent.speed = 0; //navmesh ile kapýya gidince eneymnin durmasý
+        _animator.SetTrigger("Idle"); // animatordaki ýdle anomasyonunu çalýþtýrýr
     }
 }
